3b81cb1f0f48b7caca3adabe1bbb225d3a6bc2c6
BAUDINON <baudinonicolas18@gmail.com>
2026-02-10 13:17:13 -0300

    Commit Inicial: Express, Sequelize y Docker

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { testConecction } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+testConecction();
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Bienvenido a mi Portafolio!' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`\n======================================`);
+    console.log(` Servidor corriendo en el puerto ${PORT}`);
+    console.log(` Link: http://localhost:${PORT}`);
+    console.log(`======================================\n`);
+});
---
+++ src/config/db.js
@@ -0,0 +1,27 @@
+import {Sequelize} from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false,
+    }
+);
+
+export const testConecction = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexión a la base de datos exitosa.');
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+    }
+};
+
+export default sequelize;
