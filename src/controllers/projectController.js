3a7806c1e6ef5c91a78d8fe75d66613f7836a183
BAUDINON <baudinonicolas18@gmail.com>
2026-02-27 11:46:36 -0300

    CRUD terminado

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -9,6 +9,19 @@ export const getProjects = async (req, res) => {
     }
 };
 
+export const getProjectById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const project = await Project.findByPk(id);
+        if (!project) {
+            return res.status(404).json({ message: 'Proyecto no encontrado' });
+        }
+        res.status(200).json(project);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener el proyecto', error: error.message });
+    }
+};
+
 export const CreateProject = async (req, res) => {
     try {
         const {title, description, mainImage, images, repoUrl, category} = req.body;
@@ -35,18 +48,31 @@ export const CreateProject = async (req, res) => {
     }
 };
 
-export const getProjectById = async (req, res) => {
+export const updateProject = async (req, res) => {
     try {
         const { id } = req.params;
-        const project = await Project.findByPk(id);
-        if (!project) {
-            return res.status(404).json({ message: 'Proyecto no encontrado' });
+        const { title, description, mainImage, images, repoUrl, category } = req.body;
+        const updated = await Project.update({
+            title,
+            description,
+            mainImage,
+            images,
+            repoUrl,
+            category
+        }, {
+            where: { id: id }
+        });
+
+        if (updated[0] === 0) {
+            return res.status(404).json({ message: 'Proyecto no encontrado o no actualizado' });
         }
-        res.status(200).json(project);
+        res.status(200).json({ message: 'Proyecto actualizado exitosamente' });
     } catch (error) {
-        res.status(500).json({ message: 'Error al obtener el proyecto', error: error.message });
+        res.status(500).json({ message: 'Error al actualizar el proyecto', error: error.message });
     }
 };
+
+
 
 export const deleteProject = async (req, res) => {
     try {
